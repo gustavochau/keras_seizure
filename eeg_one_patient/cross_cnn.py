76f7e99f953e2dd0620c942e9712852d700d591f
Gustavo Chau <gchau@stanford.edu>
2017-04-28 22:22:11 -0500

    cross CNN

--- eeg_one_patient/cross_cnn.py
+++ eeg_one_patient/cross_cnn.py
@@ -1,5 +1,6 @@
-'''Trains a CNN over the time segments of data and all patients, leaving one out
+'''Trains a CNN with cross patient data
 '''
+
 from __future__ import print_function
 
 from keras.models import Sequential
@@ -16,6 +17,7 @@ import numpy as np
 import math
 from keras import metrics
 from keras.callbacks import EarlyStopping
+from keras.layers import LSTM
 
 def create_class_weight(labels_dict,mu=0.15):
     total = np.sum(labels_dict.values())
@@ -34,120 +36,91 @@ def comp_metric(y_true, y_pred):
     sensitivity = 100.0*float(tp) /float((tp + fn))
     return sensitivity, fp
 
-def balance_dataset(X,Y):
-    num_pos = sum(Y == 1)
-    num_neg = sum(Y == 0)
-    X_positive = X[(Y == 1).reshape(X.shape[0]),:]
-    X_negative = X[(Y == 0).reshape(X.shape[0]),:]
-    Y_positive = Y[(Y == 1).reshape(X.shape[0]),:]
-    Y_negative = Y[(Y == 0).reshape(X.shape[0]),:]
-    ran_vec=np.random.randint(low=0, high=num_neg, size=num_pos)
-    X_negative = X_negative[ran_vec,:]
-    Y_negative = Y_negative[ran_vec, :]
-    X = np.concatenate((X_positive,X_negative))
-    Y = np.concatenate((Y_positive, Y_negative))
-    #X_ag = np.concatenate((X,Y),axis=1)
-    #X_sh = np.random.shuffle(X_ag)
+def data_generator_all_patients(main_folder, size_in, leaveout, istrain):
+    if istrain:
+        #list_all_patients = range(1, 17) + range(18, 24)
+        list_all_patients = [1,3,12,15]
+        list_leave = list()
+        list_leave.append(leaveout)
+        list_patients_training = list(set(list_all_patients) - set(list_leave))
+        print(list_patients_training)
+        X = np.zeros(shape=(0, size_in, 23))
+        Y = np.zeros(shape=(0, 1))
+        for i in list_patients_training:
+            X_temp, Y_temp = data_generator_one_patient(main_folder=main_folder, patient_number=i, size_in=size_in)
+            X = np.concatenate((X, X_temp))
+            Y = np.concatenate((Y, Y_temp))
+    else:
+        X,Y = data_generator_one_patient(main_folder = main_folder, patient_number=leaveout ,size_in=size_in)
     return X,Y
 
-def folder_to_dataset(folder, size_in):
-    list_files = listdir(folder)
-    # print(list_files)
-    num_samples = len(list_files)
-    X = np.zeros(shape=(num_samples, size_in, 23)) #dataset in theano format
-    Y = np.zeros(shape=(num_samples, 1))  # dataset in theano format
-
-    for index, file in enumerate(list_files):
-        # print(folder +'/'+ file)
-        try:
-            mat_var = loadmat(folder + '/' + file)
-        except:
-            print(folder + '/' + file)
-        # X[index,] = np.transpose(mat_var['x'])
-        X[index,:] = (mat_var['x']).reshape(size_in,  23)
-        if ((mat_var['label'] == 0)):
-            Y[index] = 0
-        else:  # ictal
-            Y[index] = 1
-    # Y = np_utils.to_categorical(Y, 2)
-    print(X.shape)
-    return (X, Y)
-
-
-def data_generator_one_patient(main_folder, patient_number,size_in, leaveout_sample, isTrain=True):
+def data_generator_one_patient(main_folder, patient_number,size_in):
     nb_classes = 2
     patient_folder = main_folder + 'chb' + str(patient_number).zfill(2)
     print(patient_folder)
     list_samples = listdir(patient_folder)
-    print(list_samples)
-    if (isTrain):
-        # take all series except for the one for testing
-        X = np.zeros(shape=(0, size_in, 23))
-        Y = np.zeros(shape=(0, 1))
-        for sample in list_samples:
-            # if is not the one to be tested
-            if (sample != ('chb' + str(patient_number).zfill(2) + '_' + str(leaveout_sample).zfill(2))):
-                print(sample)
-                (X_train, Y_train) = folder_to_dataset(patient_folder + '/' + sample,size_in)
-                # yield X_train, Y_train
-                X = np.concatenate((X, X_train))
-                Y = np.concatenate((Y, Y_train))
-        #Y = np_utils.to_categorical(Y, 2)
-        return X, Y
-    else:
-        # take only the one for testing
-        (X, Y) = folder_to_dataset(patient_folder + '/' + 'chb' + str(patient_number).zfill(2) + '_' + str(leaveout_sample).zfill(2),size_in)
-        #Y = np_utils.to_categorical(Y, 2)
-        return X, Y
-        # yield X_test, Y_test
+    print(list_samples)  # take all series except for the one for testing
+    X_pat = np.zeros(shape=(0, size_in, 23))
+    Y_pat = np.zeros(shape=(0, 1))
+    for sample in list_samples:
+        print(sample)
+        mat_var = loadmat(main_folder + 'chb' + str(patient_number).zfill(2) + '/' + sample)
+        X_train = mat_var['total_images']
+        Y_train = mat_var['total_labels']
+        # yield X_train, Y_train
+        X_pat = np.concatenate((X_pat, X_train))
+        Y_pat = np.concatenate((Y_pat, Y_train))
+        # Y = np_utils.to_categorical(Y, 2)
+    return X_pat, Y_pat
+
 
 
 if __name__ == "__main__":
-    main_folder = '/media/gustavo/TOSHIBA EXT/EEG/Data_segmentada_ds/'
+    main_folder = '/media/gustavo/TOSHIBA EXT/EEG/epilepsia/Data_segmentada_ds/'
     np.random.seed(7)
     batch_size = 1000
     num_classes = 2
-    epochs = 15
+    epochs = 50
     size_in = 256
 
     model = Sequential()
-    model.add(Conv1D(nb_filter=120, filter_length=8, input_shape=(size_in,23)))
+    model.add(Conv1D(nb_filter=30, filter_length=8, input_shape=(size_in,23)))
     model.add(Activation('relu'))
     model.add(MaxPooling1D())
     model.add(Dropout(0.2))
-    model.add(Conv1D(nb_filter=120, filter_length=5))
+    model.add(Conv1D(nb_filter=20, filter_length=5))
     model.add(Activation('relu'))
     model.add(MaxPooling1D())
     model.add(Dropout(0.2))
-    model.add(Conv1D(nb_filter=120, filter_length=3))
+    model.add(Conv1D(nb_filter=10, filter_length=3))
     model.add(Activation('relu'))
     model.add(MaxPooling1D())
     model.add(Dropout(0.2))
     model.add(Flatten())
     model.add(BatchNormalization())
-    model.add(Dense(400, activation='relu'))
+    model.add(Dense(50, activation='relu'))
     model.add(Dropout(0.2))
-    model.add(Dense(200, activation='relu'))
+    model.add(Dense(25, activation='relu'))
     model.add(Dropout(0.2))
-    model.add(Dense(80, activation='relu'))
+    model.add(Dense(10, activation='relu'))
     model.add(Dropout(0.2))
     model.add(Dense(num_classes, activation='softmax'))
 
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(loss='categorical_crossentropy',
                   optimizer='sgd')
-    los = 15
-    X_train, Y_train = data_generator_one_patient(main_folder=main_folder, patient_number=1, size_in=size_in, leaveout_sample=0,
-                                                  isTrain=True)
+    lop = 1
+    #X_train, Y_train = data_generator_one_patient(main_folder=main_folder, patient_number=1, size_in=size_in, leaveout_sample=los,
+    #                                              isTrain=True)
 
+    X_train, Y_train = data_generator_all_patients(main_folder=main_folder , size_in =size_in, leaveout = lop, istrain=True)
     print(X_train.shape)
     print(Y_train.shape)
 
-    X_test, Y_test = data_generator_one_patient(main_folder=main_folder, patient_number=1, size_in=size_in, leaveout_sample=los,
-                                                  isTrain=False)
+    X_test, Y_test = data_generator_all_patients(main_folder=main_folder, size_in=size_in, leaveout=lop, istrain=False)
+    print(X_test.shape)
+    print(Y_test.shape)
 
-    # scores = model.evaluate_generator(data_generator_mnist(False), val_samples=10000)
-    # print("Baseline Error: %.2f%%" % (100-scores[1]*100))
     model.summary()
 
     num_positive = sum(Y_train==1)
@@ -169,8 +142,8 @@ if __name__ == "__main__":
 
     history = model.fit(X_train, Y_train,
                         batch_size=batch_size,
-                        epochs=epochs, validation_split=0.1,
-                        verbose=1, class_weight=class_weight, callbacks=[early_stopping])
+                        epochs=epochs,
+                        verbose=1, class_weight=class_weight)
 
     Y_test = np_utils.to_categorical(Y_test,2)
     score = model.evaluate(X_test, Y_test, verbose=1)
